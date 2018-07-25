8cf12a53223215e85d9bca6a4425739e69504878
Frank J. Reashore <reashore@telus.net>
2018-07-25 12:59:03 -0700

    Solved problem 12

--- Problem12/Program.cs
+++ Problem12/Program.cs
@@ -8,30 +8,54 @@ namespace Problem12
         static void Main()
         {
             Console.WriteLine("Problem 12");
-            const int maxDivisors = 500;
 
-            var result = GetTriangleNumberWithDivisors(maxDivisors);
-            int number = result.Item1;
-            int triangleNumber = result.Item2;
-            int divisorCount = result.Item3;
+            TestTriangularNumbers();
+            Test();
 
-            // GetTriangleNumber(12375) = 76576500, divisorCount = 576
-            Console.WriteLine($"GetTriangleNumber({number}) = {triangleNumber}, divisorCount = {divisorCount}");
             Console.WriteLine("Done");
             Console.ReadKey();
         }
 
-        private static (int, int, int) GetTriangleNumberWithDivisors(int maxDivisors)
+        private static void TestTriangularNumbers()
         {
-            int number = 1;
-            int triangleNumber;
-            int divisorCount = 0;
+            for (ulong n = 1; n <= 10; n++)
+            {
+                ulong triangularNumber = GetTriangleNumber(n);
+                List<ulong> divisors = GetDivisors(triangularNumber);
+                Console.WriteLine($"n = {n}, triangulatNumber = {triangularNumber}");
+                divisors.ForEach(Console.WriteLine);
+            }
+        }
+
+        private static void Test()
+        {
+            List<ulong> maxDivisorsList = new List<ulong> { 2, 4, 5, 500 };
+
+            foreach (ulong maxDivisors in maxDivisorsList)
+            {
+                (ulong, ulong, ulong) result = GetTriangleNumberWithDivisors(maxDivisors);
+                ulong number = result.Item1;
+                ulong triangleNumber = result.Item2;
+                ulong divisorCount = result.Item3;
+
+                // GetTriangleNumber(12375) = 76576500, maxDivisors = 500 divisorCount = 576
+                Console.WriteLine($"GetTriangleNumber({number}) = {triangleNumber}, maxDivisors = {maxDivisors} divisorCount = {divisorCount}");
+            }
+        }
+
+        private static (ulong, ulong, ulong) GetTriangleNumberWithDivisors(ulong maxDivisors)
+        {
+            ulong number = 1;
+            ulong triangleNumber;
+            ulong divisorCount = 0;
 
             while (true)
             {
-                triangleNumber = GetTriangleNumber(number);
-                var divisors = GetDivisors(triangleNumber);
-                divisorCount = divisors.Count;
+                checked
+                {
+                    triangleNumber = GetTriangleNumber(number);
+                    divisorCount = GetDivisorsCount(triangleNumber);
+                }
 
                 if (maxDivisors < divisorCount)
                 {
@@ -44,11 +68,23 @@ namespace Problem12
             return (number, triangleNumber, divisorCount);
         }
 
-        private static List<int> GetDivisors(int number)
+        private static ulong GetTriangleNumber(ulong number)
         {
-            List<int> divisors = new List<int>();
+            ulong sum = 0;
 
-            for (int n = 1; n <= number; n++)
+            for (ulong n = 1; n <= number; n++)
+            {
+                sum += n;
+            }
+
+            return sum;
+        }
+
+        private static List<ulong> GetDivisors(ulong number)
+        {
+            List<ulong> divisors = new List<ulong>();
+
+            for (ulong n = 1; n <= number; n++)
             {
                 if (number % n == 0)
                 {
@@ -59,32 +95,19 @@ namespace Problem12
             return divisors;
         }
 
-        private static int GetTriangleNumber(int number)
+        private static ulong GetDivisorsCount(ulong number)
         {
-            int sum = 0;
+            ulong divisorsCount = 0;
 
-            for (int n = 1; n <= number; n++)
+            for (ulong n = 1; n <= number; n++)
             {
-                sum += n;
-            }
-
-            return sum;
-        }
-
-        public static List<int> GetPrimeFactors(int number)
-        {
-            var primes = new List<int>();
-
-            for (int divor = 2; divor <= number; divor++)
-            {
-                while (number % divor == 0)
+                if (number % n == 0)
                 {
-                    primes.Add(divor);
-                    number = number / divor;
+                    divisorsCount++;
                 }
             }
 
-            return primes;
+            return divisorsCount;
         }
     }
 }
