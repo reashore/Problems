a4ab391a4f922325d4b1024009a28b78e0843273
Frank J. Reashore <reashore@telus.net>
2018-07-26 08:25:49 -0700

    Solved problem 11

---
+++ Problem11/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Problem11
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Problem 11");
+
+            string grid = "";
+            int maxProduct = Solve(grid);
+            Console.WriteLine($"maxProduct = {maxProduct}");
+
+            Console.WriteLine("Done");
+            Console.ReadKey();
+        }
+
+        private static int Solve(string grid)
+        {
+
+            return 0;
+        }
+    }
+}
--- Problem23/Program.cs
+++ Problem23/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace Problem23
 {
@@ -19,64 +18,10 @@ namespace Problem23
 
             //Test();
             long result = Solve();
-            Console.WriteLine($"result = {result}");    // 4179595
+            Console.WriteLine($"result = {result}");    
 
             Console.WriteLine("Done");
             Console.ReadKey();
-        }
-
-        private static long Solve()
-        {
-            List<long> numbersWithoutAbundantAddends = new List<long>();
-
-            for (long n = 24; n <= 28123; n++)
-            {
-                (bool, long, long) result = HasAbundantAddends(n);
-                bool hasAbundantAddends = result.Item1;
-                long addend1 = result.Item2;
-                long addend2 = result.Item3;
-
-                //Console.WriteLine($"{n}, {hasAbundantAddends}, {addend1}, {addend2}");
-
-                if (n % 1000 == 0)
-                {
-                    Console.WriteLine($"{n}");
-                }
-
-                if (!hasAbundantAddends)
-                {
-                    numbersWithoutAbundantAddends.Add(n);
-                }
-            }
-
-            long sum = numbersWithoutAbundantAddends.Sum(n => n);
-
-            return sum;
-        }
-
-        private static (bool, long, long) HasAbundantAddends(long number)
-        {
-            for (long n = 12; n <= number - 12; n++)
-            {
-                long addend1 = n;
-                long addend2 = number - n;
-                // 12 <= number - n
-
-                bool abundant1 = IsAbundantNumber(addend1);
-                bool abundant2 = IsAbundantNumber(addend2);
-
-                if (abundant1 && abundant2)
-                {
-                    return (true, addend1, addend2);
-                }
-            }
-
-            return (false, 0, 0);
-        }
-
-        private static bool IsAbundantNumber(long number)
-        {
-            return GetPerfectType(number) == PerfectType.Abundant;
         }
 
         private static void Test()
@@ -88,14 +33,69 @@ namespace Problem23
             List<long> numbers = new List<long>
             {
                 12,
-                28
+                18,
+                20,
+                24,
+                28,
+                30,
+                36
             };
 
-            foreach(long number in numbers)
+            foreach (long number in numbers)
             {
                 PerfectType perfectType = GetPerfectType(number);
                 Console.WriteLine($"number = {number}, perfectType = {perfectType.ToString()}");
             }
+        }
+
+        private static long Solve()
+        {
+            long sum = 0;
+
+            for (long n = 1; n <= 28123; n++)
+            {
+                bool hasAbundantAddends;
+
+                checked
+                {
+                    hasAbundantAddends = HasAbundantAddends(n);
+                }
+
+                //Console.WriteLine($"{n}, {hasAbundantAddends}, {addend1} + {addend2} = {n}");
+
+                if (n % 1000 == 0)
+                {
+                    Console.WriteLine($"{n}");
+                }
+
+                if (!hasAbundantAddends)
+                {
+                    sum += n;
+                }
+            }
+
+            return sum;
+        }
+
+        private static bool HasAbundantAddends(long number)
+        {
+            for (long n = 12; n <= number - 12; n++)
+            {
+                long addend1 = n;
+                long addend2 = number - n;
+
+                if (IsAbundantNumber(addend1) && IsAbundantNumber(addend2))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private static bool IsAbundantNumber(long number)
+        {
+            return GetPerfectType(number) == PerfectType.Abundant;
         }
 
         private static PerfectType GetPerfectType(long number)
