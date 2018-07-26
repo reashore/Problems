3cb2f3c83d8f548d62b3dc0f93b031b16bbb2278
Frank J. Reashore <reashore@telus.net>
2018-07-25 22:49:44 -0700

    Saving

--- Problem23/Program.cs
+++ Problem23/Program.cs
@@ -17,43 +17,90 @@ namespace Problem23
         {
             Console.WriteLine("Problem 23");
 
-            Test();
-
-            Solve();
+            //Test();
+            long result = Solve();
+            Console.WriteLine($"result = {result}");    // 4179595
 
             Console.WriteLine("Done");
             Console.ReadKey();
         }
 
-        private static void Solve()
+        private static long Solve()
         {
-            //for (int n = 1; n <= 28123; n++)
-            //{
-            //    PerfectType perfectType = GetPerfectType(number);
+            List<long> numbersWithoutAbundantAddends = new List<long>();
 
+            for (long n = 24; n <= 28123; n++)
+            {
+                (bool, long, long) result = HasAbundantAddends(n);
+                bool hasAbundantAddends = result.Item1;
+                long addend1 = result.Item2;
+                long addend2 = result.Item3;
 
-            //}
+                //Console.WriteLine($"{n}, {hasAbundantAddends}, {addend1}, {addend2}");
+
+                if (n % 1000 == 0)
+                {
+                    Console.WriteLine($"{n}");
+                }
+
+                if (!hasAbundantAddends)
+                {
+                    numbersWithoutAbundantAddends.Add(n);
+                }
+            }
+
+            long sum = numbersWithoutAbundantAddends.Sum(n => n);
+
+            return sum;
+        }
+
+        private static (bool, long, long) HasAbundantAddends(long number)
+        {
+            for (long n = 12; n <= number - 12; n++)
+            {
+                long addend1 = n;
+                long addend2 = number - n;
+                // 12 <= number - n
+
+                bool abundant1 = IsAbundantNumber(addend1);
+                bool abundant2 = IsAbundantNumber(addend2);
+
+                if (abundant1 && abundant2)
+                {
+                    return (true, addend1, addend2);
+                }
+            }
+
+            return (false, 0, 0);
+        }
+
+        private static bool IsAbundantNumber(long number)
+        {
+            return GetPerfectType(number) == PerfectType.Abundant;
         }
 
         private static void Test()
         {
-            List<int> numbers = new List<int>
+            const long num = 28;
+            long sumOfProperDivisors = SumOfProperDivisors(num);
+            Console.WriteLine($"SumOfProperDivisors({num}) = {sumOfProperDivisors}");
+
+            List<long> numbers = new List<long>
             {
                 12,
                 28
             };
 
-            foreach(int number in numbers)
+            foreach(long number in numbers)
             {
                 PerfectType perfectType = GetPerfectType(number);
-                Console.WriteLine($"perfectType = {perfectType.ToString()}");
+                Console.WriteLine($"number = {number}, perfectType = {perfectType.ToString()}");
             }
         }
 
-        private static PerfectType GetPerfectType(int number)
+        private static PerfectType GetPerfectType(long number)
         {
-            List<int> properDivisors = GetProperDivisors(number);
-            int sumOfDivisors = properDivisors.Sum(n => n);
+            long sumOfDivisors = SumOfProperDivisors(number);
 
             if (number == sumOfDivisors)
             {
@@ -69,19 +116,19 @@ namespace Problem23
             }
         }
 
-        private static List<int> GetProperDivisors(int number)
+        private static long SumOfProperDivisors(long number)
         {
-            List<int> divisors = new List<int>();
+            long sum = 0;
 
-            for (int n = 1; n < number; n++)
+            for (long n = 1; n < number; n++)
             {
                 if (number % n == 0)
                 {
-                    divisors.Add(n);
+                    sum += n;
                 }
             }
 
-            return divisors;
+            return sum;
         }
     }
 }
