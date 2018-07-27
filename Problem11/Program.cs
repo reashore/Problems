0d65483f84a8fa700b0f42c27f0db41271f40cc2
Frank J. Reashore <reashore@telus.net>
2018-07-26 23:43:22 -0700

    saving

--- Problem11/Program.cs
+++ Problem11/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Problem11
 {
@@ -8,18 +10,280 @@ namespace Problem11
         {
             Console.WriteLine("Problem 11");
 
-            string grid = "";
-            int maxProduct = Solve(grid);
+            const int arraySize = 20;
+            const int size = 4;
+            int[,] array = CreateArray(arraySize);
+            int maxProduct = Solve(arraySize, size, array);
+
             Console.WriteLine($"maxProduct = {maxProduct}");
 
             Console.WriteLine("Done");
             Console.ReadKey();
         }
 
-        private static int Solve(string grid)
+        private static int[,] CreateArray(int size)
         {
+            string gridString =
+                "08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08 " +
+                "49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00 " +
+                "81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65 " +
+                "52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91 " +
+                "22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80 " +
+                "24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50 " +
+                "32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70 " +
+                "67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21 " +
+                "24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72 " +
+                "21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95 " +
+                "78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92 " +
+                "16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57 " +
+                "86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58 " +
+                "19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 04 89 55 40 " +
+                "04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 33 27 98 66 " +
+                "88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 69 " +
+                "04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 40 62 76 36 " +
+                "20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16 " +
+                "20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54 " +
+                "01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48";
 
-            return 0;
+            string[] gridArray = gridString.Split(new char[] { ' ' });
+            List<int> gridList = new List<int>();
+
+            foreach (string digitString in gridArray)
+            {
+                int digit = Convert.ToInt32(digitString);
+                gridList.Add(digit);
+            }
+
+            List<int> rowList;
+            int[,] array = new int[size, size];
+
+            for (int row = 0; row < size; row++)
+            {
+                int skip = row * size;
+                rowList = gridList.Skip(skip).Take(size).ToList();
+
+                for (int col = 0; col < size; col++)
+                {
+                    array[row, col] = rowList[col];
+                }
+            }
+
+            return array;
+        }
+
+        private static int Solve(int arraySize, int size, int[,] array)
+        {
+            int maxArraySum = 0;
+            int maxSubArraySum = 0;
+
+            // ****** Change sum to product **************
+
+            // row - 1 + 4 < 5
+            for (int row = 0; row - 1 + size < arraySize; row++)
+            {
+                for (int col = 0; col - 1 + size < arraySize; col++)
+                {
+                    maxSubArraySum = GetMaxSubArraySum(size, row, col, array);
+
+                    if (maxSubArraySum > maxArraySum)
+                    {
+                        maxArraySum = maxSubArraySum;
+                    }
+                }
+            }
+
+            return maxArraySum;
+        }
+
+        private static int GetMaxSubArraySum(int size, int rowOffset, int colOffset, int[,] array)
+        {
+            int maxRowSum = GetMaxRowSum(size, rowOffset, colOffset, array);
+            int maxColSum = GetMaxColSum(size, rowOffset, colOffset, array);
+            int diagonal1Sum = GetDiagonal1Sum(size, rowOffset, colOffset, array);
+            int diagonal2Sum = GetDiagonal2Sum(size, rowOffset, colOffset, array);
+
+            int maxSum = maxRowSum;
+
+            if (maxColSum > maxSum)
+            {
+                maxSum = maxColSum;
+            }
+
+            if (diagonal1Sum > maxSum)
+            {
+                maxSum = diagonal1Sum;
+            }
+
+            if (diagonal2Sum > maxSum)
+            {
+                maxSum = diagonal2Sum;
+            }
+
+            return maxSum;
+
+            #region Old code
+            //***********************************************************
+
+            // sum rows
+            // a[0, 0] + a[0, 1] + a[0, 2] + a[0, 3]
+            //for (int row = 0; row < Size; row++)
+            //{
+            //    sum = 0;
+
+            //    for (int col = 0; col < Size; col++)
+            //    {
+            //        sum += array[row + rowOffset, col + colOffset];
+            //    }
+
+            //    Console.WriteLine($"sum = {sum}");
+
+            //    if (sum > maxSum)
+            //    {
+            //        maxSum = sum;
+            //    }
+            //}
+
+            //Console.WriteLine();
+
+            //***********************************************************
+
+            // sum columns
+            // a[0, 0] + a[1, 0] + a[2, 0] + a[3, 0]
+            //for (int col = 0; col < Size; col++)
+            //{
+            //    sum = 0;
+
+            //    for (int row = 0; row < Size; row++)
+            //    {
+            //        sum += array[row + rowOffset, col + colOffset];
+            //    }
+
+            //    Console.WriteLine($"sum = {sum}");
+
+            //    if (sum > maxSum)
+            //    {
+            //        maxSum = sum;
+            //    }
+            //}
+
+            Console.WriteLine();
+
+            //***********************************************************
+
+            //sum = 0;
+
+            // sum diagonal 1
+            // a[0, 0] + a[1, 1] + a[2, 2] + a[3, 3]
+            //for (int n = 0; n < Size; n++)
+            //{
+            //    sum += array[n + rowOffset, n + colOffset];
+            //}
+            //Console.WriteLine($"sum = {sum}");
+
+            //if (sum > maxSum)
+            //{
+            //    maxSum = sum;
+            //}
+
+            //***********************************************************
+
+            //sum = 0;
+
+            // sum diagonal 2
+            // a[3, 0] + a[2, 1] + a[1, 2] + a[0, 3]
+            //for (int n = 0; n < Size; n++)
+            //{
+            //    sum += array[Size - n - 1 + rowOffset, n + colOffset];
+            //}
+            //Console.WriteLine($"sum = {sum}");
+
+            //if (sum > maxSum)
+            //{
+            //    maxSum = sum;
+            //}
+            #endregion
+        }
+
+        private static int GetMaxRowSum(int size, int rowOffset, int colOffset, int[,] array)
+        {
+            int sum;
+            int maxSum = 0;
+
+            // a[0, 0] + a[0, 1] + a[0, 2] + a[0, 3]
+            for (int row = 0; row < size; row++)
+            {
+                sum = 0;
+
+                for (int col = 0; col < size; col++)
+                {
+                    sum += array[row + rowOffset, col + colOffset];
+                }
+
+                Console.WriteLine($"row sum = {sum}");
+
+                if (sum > maxSum)
+                {
+                    maxSum = sum;
+                }
+            }
+
+            return maxSum;
+        }
+
+        private static int GetMaxColSum(int size, int rowOffset, int colOffset, int[,] array)
+        {
+            int sum;
+            int maxSum = 0;
+
+            // a[0, 0] + a[1, 0] + a[2, 0] + a[3, 0]
+            for (int col = 0; col < size; col++)
+            {
+                sum = 0;
+
+                for (int row = 0; row < size; row++)
+                {
+                    sum += array[row + rowOffset, col + colOffset];
+                }
+
+                Console.WriteLine($"col sum = {sum}");
+
+                if (sum > maxSum)
+                {
+                    maxSum = sum;
+                }
+            }
+
+            return maxSum;
+        }
+        
+        private static int GetDiagonal1Sum(int size, int rowOffset, int colOffset, int[,] array)
+        {
+            int sum = 0;
+
+            // a[0, 0] + a[1, 1] + a[2, 2] + a[3, 3]
+            for (int n = 0; n < size; n++)
+            {
+                sum += array[n + rowOffset, n + colOffset];
+            }
+
+            Console.WriteLine($"diagonal1 sum = {sum}");
+
+            return sum;
+        }
+
+        private static int GetDiagonal2Sum(int size, int rowOffset, int colOffset, int[,] array)
+        {
+            int sum = 0;
+
+            // a[3, 0] + a[2, 1] + a[1, 2] + a[0, 3]
+            for (int n = 0; n < size; n++)
+            {
+                sum += array[size - n - 1 + rowOffset, n + colOffset];
+            }
+
+            Console.WriteLine($"diagonal sum = {sum}");
+
+            return sum;
         }
     }
 }
