b7c2d8d6edfc0d0e0bfbcb9d846d3f08b1bf2e5b
Frank J. Reashore <reashore@telus.net>
2018-07-28 15:57:02 -0700

    Solved problem 28

---
+++ Problem18/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem18
+{
+    class Node
+    {
+        public Node()
+        {
+
+        }
+
+        public Node(int value, int leftvalue, int rightValue)
+        {
+            Value = value;
+            LeftValue = leftvalue;
+            RightValue = rightValue;
+        }
+
+        public int Value { get; set; }
+        public int LeftValue { get; set; }
+        public int RightValue { get; set; }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Problem 18");
+
+            Node root = LoadBinaryTree();
+
+            Console.WriteLine("Done");
+            Console.ReadKey();
+        }
+
+        private static Node LoadBinaryTree()
+        {
+            string treeString =
+                "75 " +
+                "95 64 " +
+                "17 47 82 " +
+                "18 35 87 10 " +
+                "20 04 82 47 65 " +
+                "19 01 23 75 03 34 " +
+                "88 02 77 73 07 63 67 " +
+                "99 65 04 28 06 16 70 92 " +
+                "41 41 26 56 83 40 80 70 33 " +
+                "41 48 72 33 47 32 37 16 94 29 " +
+                "53 71 44 65 25 43 91 52 97 51 14 " +
+                "70 11 33 28 77 73 17 78 39 68 17 57 " +
+                "91 71 52 38 17 14 91 43 58 50 27 29 48 " +
+                "63 66 04 68 89 53 67 30 73 16 69 87 40 31 " +
+                "04 62 98 27 23 09 70 98 73 93 38 53 60 04 23";
+
+            string[] treeArray = treeString.Split(new char[] { ' ' });
+            List<int> treeList = new List<int>();
+
+            foreach (string valueString in treeArray)
+            {
+                int value = Convert.ToInt32(valueString);
+                treeList.Add(value);
+            }
+
+            const int treeDepth = 15;
+            Node root = new Node();
+
+            #region old
+            Node current = root;
+            int skip = 0;
+            int take;
+
+            for (int n = 1; n <= treeDepth; n++)
+            {
+                take = treeDepth;
+                var list = treeList.Skip(skip).Take(take);
+                //CreateChildNodes(current);
+
+
+                skip += take;
+            }
+            #endregion
+
+            root.Value = 75;
+            CreateChildNodes(root, treeList, treeDepth);
+
+
+            return new Node(1, 1, 1);
+        }
+
+        private static void CreateChildNodes(Node current, List<int> treeList, int depth)
+        {
+            
+        }
+    }
+}
---
+++ Problem28/Program.cs
@@ -0,0 +1,214 @@
+﻿using System;
+
+namespace Problem28
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Problem 28");
+
+            const int size = 1001;
+            long sumBothDiagonals = Solve(size);
+            Console.WriteLine($"sumBothDiagonals = {sumBothDiagonals}");
+
+            Console.WriteLine("Done");
+            Console.ReadKey();
+        }
+
+        private static long Solve(int size)
+        {
+            int[,] matrix = new int[size, size];
+
+            matrix = CreateMatrixSpiral(size, matrix);
+            //PrintMatrix(size, matrix);
+            long sumBothDiagonals = SumBothDiagonals(size, matrix);
+
+            return sumBothDiagonals;
+        }
+
+        private static int[,] CreateMatrixSpiral(int size, int[,] matrix)
+        {
+            if (size % 2 == 0)
+            {
+                throw new ArgumentException("Matrix size must be odd");
+            }
+
+            int middle = (size - 1) / 2;
+            int nextValue = 1;
+            matrix[middle, middle] = nextValue++;
+
+            // boxSize, iterations
+            // 3, 2
+            // 5, 4
+            // 7, 6
+            // boxSize => boxSize - 1
+
+            for (int boxSize = 3; boxSize <= size; boxSize += 2)
+            {
+                // boxSize, rowOffset
+                // 3, 0
+                // 5, -1
+                // 7, -2
+                // boxSize => -boxSize / 2 + 1
+
+                // boxSize, colOffset
+                // 3, 1
+                // 5, 2
+                // 7, 3
+                // boxSize = boxSize / 2
+
+                int numIterations = boxSize - 1;
+
+                // top -> bottom
+                for (int n = 0; n < numIterations; n++)
+                {
+                    int rowOffset = -boxSize / 2 + 1;
+                    int colOffset = boxSize / 2;
+
+                    int row = middle + rowOffset + n;
+                    int col = middle + colOffset;
+
+                    matrix[row, col] = nextValue++;
+                }
+
+                // boxSize, rowOffset
+                // 3, -1
+                // 5, -2
+                // 7, -3
+                // boxSize => -boxSize / 2
+
+                // boxSize, colOffset
+                // 3, 0
+                // 5, 1
+                // 7, 2
+                // 9, 3
+                // boxSize -> boxSize / 2 - 1
+
+                // right -> left
+                for (int n = 0; n < numIterations; n++)
+                {
+                    int rowOffset = boxSize / 2;
+                    int colOffset = boxSize / 2 - 1;
+
+                    int row = middle + rowOffset;
+                    int col = middle + colOffset - n;
+
+                    matrix[row, col] = nextValue++;
+                }
+
+                // boxSize, rowOffset
+                // 3, 0
+                // 5, 1
+                // 7, 2
+                // 9, 3
+                // boxSize => boxSize / 2 - 1
+
+                // boxSize, colOffset
+                // 3, -1
+                // 5, -2
+                // 7, -3
+                // boxSize => -boxSize / 2
+
+                // bottom -> top
+                for (int n = 0; n < numIterations; n++)
+                {
+                    int rowOffset = boxSize / 2 - 1;
+                    int colOffset = -boxSize / 2;
+
+                    int row = middle + rowOffset - n;
+                    int col = middle + colOffset;
+
+                    matrix[row, col] = nextValue++;
+                }
+
+                // boxSize, rowOffset
+                // 3, -1
+                // 5, -2
+                // 7, -3
+                // boxSize => -boxSize / 2
+
+                // boxSize, colOffset
+                // 3, 0
+                // 5, -1
+                // 7, -2
+                // boxSize => -boxSize / 2 + 1
+
+                // left -> right
+                for (int n = 0; n < numIterations; n++)
+                {
+                    int rowOffset = -boxSize / 2;
+                    int colOffset = -boxSize / 2 + 1;
+
+                    int row = middle + rowOffset;
+                    int col = middle + colOffset + n;
+
+                    matrix[row, col] = nextValue++;
+                }
+            }
+
+            return matrix;
+        }
+
+        private static long SumBothDiagonals(int size, int[,] matrix)
+        {
+            long sumDiagonal1 = SumDiagonal1(size, matrix);
+            long sumDiagonal2 = SumDiagonal2(size, matrix);
+            long sumBothDiagonals = sumDiagonal1 + sumDiagonal2 - 1;
+
+            return sumBothDiagonals;
+        }
+
+        private static long SumDiagonal1(int size, int[,] matrix)
+        {
+            long sum = 0;
+
+            for (int row = 0; row < size; row++)
+            {
+                sum += matrix[row, row];
+            }
+
+            return sum;
+        }
+
+        private static long SumDiagonal2(int size, int[,] matrix)
+        {
+            long sum = 0;
+
+            for (int n = 0; n < size; n++)
+            {
+                 sum += matrix[n, size - 1 - n];
+            }
+
+            return sum;
+        }
+
+        private static void InitializeMatrixToZeros(int size, int[,] matrix)
+        {
+            for (int row = 0; row < size; row++)
+            {
+                for (int col = 0; col < size; col++)
+                {
+                    matrix[row, col] = 0;
+                }
+            }
+        }
+
+        private static void PrintMatrix(int size, int[,] matrix)
+        {
+            for (int row = 0; row < size; row++)
+            {
+                string matrixRow = "";
+
+                for (int col = 0; col < size; col++)
+                {
+                    matrixRow += $"{matrix[row, col], 4}, ";
+                }
+
+                Console.WriteLine(matrixRow);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
