54384d89f931aa327369f0732366f02923f78a78
Frank J. Reashore <reashore@telus.net>
2018-08-02 12:30:57 -0700

    Solved Problem 45

---
+++ Problem17/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Problem17
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Problem 17");
+
+            SortedDictionary<int, string> numbersDictionary = new SortedDictionary<int, string>();
+
+            AssignBasicNumbers(numbersDictionary);
+            AssignNumbers21to99(numbersDictionary);
+            AssignNumbers101to999(numbersDictionary);
+            Print(numbersDictionary);
+            int letterCount = CountLetters(numbersDictionary);
+            Console.WriteLine($"letterCount = {letterCount}");      // 21124
+
+            Console.WriteLine("Done");
+            Console.ReadKey();
+        }
+
+        private static void AssignBasicNumbers(SortedDictionary<int, string> numbersDictionary)
+        {
+            numbersDictionary[1] = "one";
+            numbersDictionary[2] = "two";
+            numbersDictionary[3] = "three";
+            numbersDictionary[4] = "four";
+            numbersDictionary[5] = "five";
+            numbersDictionary[6] = "six";
+            numbersDictionary[7] = "seven";
+            numbersDictionary[8] = "eight";
+            numbersDictionary[9] = "nine";
+            numbersDictionary[10] = "ten";
+
+            numbersDictionary[11] = "eleven";
+            numbersDictionary[12] = "twelve";
+            numbersDictionary[13] = "thirteen";
+            numbersDictionary[14] = "fourteen";
+            numbersDictionary[15] = "fifteen";
+            numbersDictionary[16] = "sixteen";
+            numbersDictionary[17] = "seventeen";
+            numbersDictionary[18] = "eighteen";
+            numbersDictionary[19] = "nineteen";
+
+            numbersDictionary[20] = "twenty";
+            numbersDictionary[30] = "thirty";
+            numbersDictionary[40] = "forty";
+            numbersDictionary[50] = "fifty";
+            numbersDictionary[60] = "sixty";
+            numbersDictionary[70] = "seventy";
+            numbersDictionary[80] = "eighty";
+            numbersDictionary[90] = "ninety";
+
+            numbersDictionary[1000] = "one thousand";
+        }
+
+        private static void AssignNumbers21to99(SortedDictionary<int, string> numbersDictionary)
+        {
+            for (int start = 20; start <= 90; start += 10)
+            {
+                string prefix = numbersDictionary[start] + " ";
+
+                for (int n = 1; n <= 9; n++)
+                {
+                    numbersDictionary[start + n] = prefix + numbersDictionary[n];
+                }
+            }
+        }
+
+        private static void AssignNumbers101to999(SortedDictionary<int, string> numbersDictionary)
+        {
+            for (int start = 100; start <= 900; start += 100)
+            {
+                string hundred = numbersDictionary[start / 100];
+                numbersDictionary[start] = hundred + " hundred";
+                string prefix = hundred + " hundred and ";
+
+                for (int n = 1; n <= 99; n++)
+                {
+                    numbersDictionary[start + n] = prefix + numbersDictionary[n];
+                }
+            }
+        }
+
+        private static void Print(SortedDictionary<int, string> numbersDictionary)
+        {
+            foreach (var item in numbersDictionary)
+            {
+                Console.WriteLine($"{item.Key} = {item.Value}");
+            }
+        }
+
+        private static int CountLetters(SortedDictionary<int, string> numbersDictionary)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            foreach (var item in numbersDictionary)
+            {
+                stringBuilder.Append(item.Value);
+            }
+
+            string allNumbersFrom1to1000 = stringBuilder.ToString();
+            allNumbersFrom1to1000 = allNumbersFrom1to1000.Replace(" ", "");
+            int letterCount = allNumbersFrom1to1000.Length;
+
+            return letterCount;
+        }
+    }
+}
--- Problem24/Program.cs
+++ Problem24/Program.cs
@@ -1,17 +1,103 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Problem24
 {
     class Program
     {
+        // https://stackoverflow.com/questions/756055/listing-all-permutations-of-a-string-integer
+
         static void Main()
         {
             Console.WriteLine("Problem 24");
 
+            //*********************************
+
+            List<int> input = new List<int> { 0, 1, 2 };
+            IEnumerable<List<int>> permutations = Permutate(input);
+
+            foreach (List<int> permutation in permutations)
+            {
+                var temp = permutation;
+
+                string permutationString = "";
+
+                foreach( int item in permutation)
+                {
+                    permutationString += item.ToString() + ", ";
+                }
+
+                Console.WriteLine(permutationString);
+
+            }
 
 
-            Console.WriteLine("Donw");
+            //*********************************
+
+
+            string value = "012";
+            char[] valueArray = value.ToCharArray();
+            int firstIndex = 0;
+            int lastIndex = valueArray.Length - 1;
+            //Permute(valueArray, 0, 2);
+            Permute(valueArray, firstIndex, lastIndex);
+
+
+            Console.WriteLine("Done");
             Console.ReadKey();
+        }
+
+        static void Permute(char[] array, int i, int n)
+        {
+            int j;
+
+            if (i == n)
+            {
+                Console.WriteLine(array);
+            }
+            else
+            {
+                for (j = i; j <= n; j++)
+                {
+                    Swap(ref array[i], ref array[j]);
+                    Permute(array, i + 1, n);
+                    Swap(ref array[i], ref array[j]); //backtrack
+                }
+            }
+        }
+
+        static void Swap(ref char a, ref char b)
+        {
+            char tmp = a;
+            a = b;
+            b = tmp;
+        }
+
+        //*********************
+        // https://stackoverflow.com/questions/2710713/algorithm-to-generate-all-possible-permutations-of-a-list
+
+        private static IEnumerable<List<T>> Permutate<T>(List<T> input)
+        {
+            if (input.Count == 2) // this are permutations of array of size 2
+            {
+                yield return new List<T>(input);
+                yield return new List<T> { input[1], input[0] };
+            }
+            else
+            {
+                foreach (T elem in input) 
+                {
+                    var rlist = new List<T>(input); // creating subarray = array
+                    rlist.Remove(elem); // removing element
+
+                    foreach (List<T> retlist in Permutate(rlist))
+                    {
+                        retlist.Insert(0, elem); // inserting the element at pos 0
+                        yield return retlist;
+                    }
+                }
+            }
         }
     }
 }
