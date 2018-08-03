27343a1128f21918fc93586082763a55ad19f0f1
Frank J. Reashore <reashore@telus.net>
2018-08-03 15:39:27 -0700

    Code cleanup

--- Problem24/Program.cs
+++ Problem24/Program.cs
@@ -1,21 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Problem24
 {
+    // https://stackoverflow.com/questions/2710713/algorithm-to-generate-all-possible-permutations-of-a-list
+
     // ReSharper disable once ClassNeverInstantiated.Global
     internal class Program
     {
-        // https://stackoverflow.com/questions/756055/listing-all-permutations-of-a-string-integer
-
         private static void Main()
         {
             Console.WriteLine("Problem 24");
 
-            //*********************************
-
-            List<int> input = new List<int> { 0, 1, 2 };
-            IEnumerable<List<int>> permutations = Permutate(input);
+            List<int> sequence = new List<int> { 0, 1, 2 };
+            IEnumerable<List<int>> permutations = Permutate(sequence);
+            //var orderedPermutations = permutations.OrderBy(n => n);
 
             foreach (List<int> permutation in permutations)
             {
@@ -29,71 +29,73 @@ namespace Problem24
                 Console.WriteLine(permutationString);
             }
 
-
-            //*********************************
-
-
-            const string value = "012";
-            char[] valueArray = value.ToCharArray();
-            const int firstIndex = 0;
-            int lastIndex = valueArray.Length - 1;
-            //Permute(valueArray, 0, 2);
-            Permute(valueArray, firstIndex, lastIndex);
-
-
             Console.WriteLine("Done");
             Console.ReadKey();
         }
 
-        private static void Permute(char[] array, int i, int n)
+        private static IEnumerable<List<T>> Permutate<T>(IReadOnlyList<T> sequence)
         {
-            if (i == n)
+
+            if (sequence.Count == 2) 
             {
-                Console.WriteLine(array);
+                yield return new List<T>(sequence);
+                yield return new List<T> { sequence[1], sequence[0] };
             }
             else
             {
-                int j;
-                for (j = i; j <= n; j++)
+                foreach (T element in sequence) 
                 {
-                    Swap(ref array[i], ref array[j]);
-                    Permute(array, i + 1, n);
-                    Swap(ref array[i], ref array[j]); //backtrack
-                }
-            }
-        }
+                    List<T> rList = new List<T>(sequence); 
+                    rList.Remove(element); 
 
-        private static void Swap(ref char a, ref char b)
-        {
-            char tmp = a;
-            a = b;
-            b = tmp;
-        }
-
-        //*********************
-        // https://stackoverflow.com/questions/2710713/algorithm-to-generate-all-possible-permutations-of-a-list
-
-        private static IEnumerable<List<T>> Permutate<T>(IReadOnlyList<T> input)
-        {
-            if (input.Count == 2) 
-            {
-                yield return new List<T>(input);
-                yield return new List<T> { input[1], input[0] };
-            }
-            else
-            {
-                foreach (T elem in input) 
-                {
-                    List<T> rlist = new List<T>(input); 
-                    rlist.Remove(elem); 
-
-                    foreach (List<T> retlist in Permutate(rlist))
+                    foreach (List<T> retList in Permutate(rList))
                     {
-                        retlist.Insert(0, elem); 
-                        yield return retlist;
+                        retList.Insert(0, element); 
+                        yield return retList;
                     }
                 }
             }
         }
+
+        #region OldCode
+
+        // https://stackoverflow.com/questions/756055/listing-all-permutations-of-a-string-integer
+
+
+        //const string value = "012";
+        //char[] valueArray = value.ToCharArray();
+        //const int firstIndex = 0;
+        //int lastIndex = valueArray.Length - 1;
+        ////Permute(valueArray, 0, 2);
+        //Permute(valueArray, firstIndex, lastIndex);
+
+
+        //private static void Permute(char[] array, int i, int n)
+        //{
+        //    if (i == n)
+        //    {
+        //        Console.WriteLine(array);
+        //    }
+        //    else
+        //    {
+        //        int j;
+        //        for (j = i; j <= n; j++)
+        //        {
+        //            Swap(ref array[i], ref array[j]);
+        //            Permute(array, i + 1, n);
+        //            Swap(ref array[i], ref array[j]); //backtrack
+        //        }
+        //    }
+        //}
+
+        //private static void Swap(ref char a, ref char b)
+        //{
+        //    char tmp = a;
+        //    a = b;
+        //    b = tmp;
+        //}
+
+
+        #endregion
     }
 }
