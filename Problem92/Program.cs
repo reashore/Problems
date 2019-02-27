72323fe8c0f485f3b171be7f6482db170979b810
Frank J. Reashore <reashore@telus.net>
2019-02-27 13:55:01 -0800

    Started problem 92

---
+++ Problem92/Program.cs
@@ -0,0 +1,23 @@
+﻿using static System.Console;
+
+namespace Problem92
+{
+    public class Program
+    {
+        static void Main()
+        {
+            WriteLine("Problem 92");
+
+            int answer = Solve();
+            WriteLine($"answer = {answer}");
+            
+            WriteLine("Done");
+            ReadKey();
+        }
+
+        private static int Solve()
+        {
+            return 0;
+        }
+    }
+}
