05bc591691dded845f1f5c2b69a92c61d17ca10c
Frank J. Reashore <reashore@telus.net>
2019-03-13 09:26:43 -0700

    Started problem 51

---
+++ Problem51/Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+
+namespace Problem51
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            WriteLine("Problem 51");
+        }
+    }
+}
