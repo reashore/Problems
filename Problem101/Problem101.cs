6ee789bd0226178a7658a0126e987b5e500fc781
Frank J. Reashore <reashore@telus.net>
2021-12-11 23:55:25 -0800

    Solved Problem101

---
+++ Problem101/Problem101.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Problem101
+{
+    public class Problem101
+    {
+        public static long Solve(int n)
+        {
+            return (long) n * n * n;
+        }
+
+        //public static int Solve(int n)
+        //{
+        //    int groupFirstIndex = n * (n - 1) + 1;
+        //    int sum = groupFirstIndex * n + n * (n - 1);
+
+        //    return sum;
+        //}
+
+        public static void Test()
+        {
+            for (int n = 1; n <= 6; n++)
+            {
+                int groupFirstIndex = n * (n - 1) + 1;
+                int sum = groupFirstIndex * n + n * (n - 1);
+                int sum2 = n * n * n;
+
+                string message = $"groupFirstIndex = {groupFirstIndex}, sum = {sum}, sum2 = {sum2}";
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
---
+++ Problem101/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+
+namespace Problem101
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            //Problem101.Test();
+
+            WriteLine("Problem 101");
+            int n = 3;
+            long answer = Problem101.Solve(n);
+            WriteLine($"answer = {answer}");            // 27
+        }
+    }
+}
