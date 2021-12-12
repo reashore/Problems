62a069b2b815c505355fc8aa62d61f6caeac3142
Frank J. Reashore <reashore@telus.net>
2021-12-12 11:48:50 -0800

    Saving

---
+++ Problem102/Problem102.cs
@@ -0,0 +1,23 @@
+﻿namespace Problem102
+{
+    public class Problem102
+    {
+        public static long Solve(int n)
+        {
+            checked
+            {
+                long power = 1;
+
+                for (int i = 1; i <= n; i++)
+                {
+                    power *= 2;
+                    power %= 100_000;
+                }
+
+                long answer = power - 1;
+
+                return answer;
+            }
+        }
+    }
+}
---
+++ Problem102/Program.cs
@@ -0,0 +1,23 @@
+﻿using static System.Console;
+
+namespace Problem102
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            WriteLine("Problem 102");
+            int n = 2;
+            long answer = Problem102.Solve(n);
+            WriteLine($"answer = {answer}");            // 3
+
+            n = 3;
+            answer = Problem102.Solve(n);
+            WriteLine($"answer = {answer}");            // 7
+
+            n = 663;
+            answer = Problem102.Solve(n);
+            WriteLine($"answer = {answer}");            // 4447
+        }
+    }
+}
