80ac4e1585b9722c54874dabaa874891b287d677
Frank J. Reashore <reashore@telus.net>
2021-12-12 15:03:44 -0800

    Saving

---
+++ Problem103/Problem103.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace Problem103
+{
+    public class Problem103
+    {
+        public static int Solve(List<int> tempList)
+        {
+            int minTemp = int.MaxValue;
+
+            foreach (int temp in tempList)
+            {
+                if (Abs(temp) < Abs(minTemp))
+                {
+                    minTemp = temp;
+                }
+
+                if (Abs(temp) == Abs(minTemp))
+                {
+                    if (temp > minTemp)
+                    {
+                        minTemp = temp;
+                    }
+                }
+            }
+
+            return minTemp;
+        }
+
+        private static int Abs(int x)
+        {
+            if (x < 0)
+            {
+                return -x;
+            }
+
+            return x;
+        }
+    }
+}
---
+++ Problem103/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using static System.Console;
+
+namespace Problem103
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            WriteLine("Problem 103");
+            List<int> tempList = new List<int>()
+            {
+                4, -2, 6, 2, -2, -4, 3
+            };
+            long answer = Problem103.Solve(tempList);
+            WriteLine($"answer = {answer}");            // 2
+        }
+    }
+}
+
